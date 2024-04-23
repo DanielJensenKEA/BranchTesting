6a3a24fb51de7bb444b1874a17080c7825eb6ddd
DanielJensenKEA <daje0003@stud.kea.dk>
2024-04-23 15:12:03 +0200

    eat merge

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Dog dog = new Dog();
         Cat cat = new Cat();
+
+        cat.eat("fish");
+        dog.eat("meat");
     }
 }
